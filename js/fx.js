6c3ba3cee8b65afb3f2ce710e6d0cfbe5afb1303
Iwani <iwanikhalid@gmail.com>
2014-05-21 14:56:24 +0800

    Resolved #31 yay!

--- js/fx.js
+++ js/fx.js
@@ -1,4 +1,23 @@
 jQuery(document).ready(function($) {
+
+	// Twitter
+	  $('.popup').click(function(event) {
+		var width  = 575,
+			height = 400,
+			left   = ($(window).width()  - width)  / 2,
+			top    = ($(window).height() - height) / 2,
+			url    = this.href,
+			opts   = 'status=1' +
+					 ',width='  + width  +
+					 ',height=' + height +
+					 ',top='    + top    +
+					 ',left='   + left;
+		
+		window.open(url, 'twitter', opts);
+	 
+		return false;
+	  });
+
 	$(function() {
 		$(window).scroll(function() {
 			if($(this).scrollTop() != 0) {
